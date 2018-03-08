9684bc112ff503c52b1f90333b71c1f857322105
Paul Thompson <paul.thompson@uetechnologies.com>
2018-03-07 21:23:45 -0800

    make a proper module out of saleae

---
+++ saleae.h
@@ -0,0 +1,42 @@
+#ifndef SALEAE
+#define SALEAE
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void proxy_D0( const int set);
+void proxy_D1( const int set);
+void proxy_D2( const int set);
+void proxy_D3( const int set);
+void proxy_D4( const int set);
+void proxy_D5( const int set);
+void proxy_D6( const int set);
+void proxy_D7( const int set);
+void proxy_D8( const int set);
+void proxy_D9( const int set);
+void proxy_D10(const int set);
+void proxy_D11(const int set);
+void proxy_D12(const int set);
+void proxy_D13(const int set);
+
+void pulse_D0();
+void pulse_D1();
+void pulse_D2();
+void pulse_D3();
+void pulse_D4();
+void pulse_D5();
+void pulse_D6();
+void pulse_D7();
+void pulse_D8();
+void pulse_D9();
+void pulse_D10();
+void pulse_D11();
+void pulse_D12();
+void pulse_D13();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SALEAE */
